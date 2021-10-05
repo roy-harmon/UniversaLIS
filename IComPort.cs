6bfbb4161d8af135d40e74ab7da07730be3650a5
roy-harmon <harmon58@live.marshall.edu>
2021-10-05 00:15:28 -0500

    Add interface to give serial and TCP connections the same public methods (much work still needed to make this functional)

---
+++ IComPort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversaLIS
+{
+     interface IComPort
+     {
+          public void Send();
+          public void Open();
+          public void Close();
+          //public void Write(byte[] buffer, int offset, int count);
+          //public int Read(byte[] buffer, int offset, int count);
+     }
+}
